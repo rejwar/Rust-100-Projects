c73186f93451e78029c252f246ca17b3f4f722c0
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2024-02-07 12:04:00 +0600

    Commit message for 7 Tarikh

--- ToDoList/src/main.rs
+++ ToDoList/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::{self , File};
+use std::io::{self, Write};
+use serde::{Serialize , Deserialize};
+
+#[derive(Serialize , Deserialize , Debug)]
+struct Task {
+    id: usize,
+    description: String,
+    completed: bool,
+}
+
+fn mut() {
+    let mut Task: Vec<Task> = load_tasks();
+
+    loop {
+        println!("\n To do list Menu");
+        println!("1. Add Task");
+        println!("2. View Tasks");
+        println!("3 . Mark Task as Complete");
+        println!("4. Delete Task");
+        println!("5. Exit");
+    }
 }
