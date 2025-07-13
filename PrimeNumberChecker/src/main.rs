172e93d354e68b0843fa7d3097e15bbd506408fa
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-07-14 03:16:12 +0600

    Updated

--- PrimeNumberChecker/src/main.rs
+++ PrimeNumberChecker/src/main.rs
@@ -1,13 +1,13 @@
 use std::io;
 
 fn main() {
-    println!("Prime Number  Checker ");
-    println!("Enter a positive integer to check if it's prime or not ");
+    println!("Prime Number Checker");
+    println!("Enter a positive integer to check if it's prime or not");
 
     let number = match get_input_as_u32() {
         Some(value) => value,
         None => {
-            println!("Invalid input . Please enter a positive integer"),
+            println!("Invalid input. Please enter a positive integer");
             return;
         }
     };
@@ -18,9 +18,38 @@ fn main() {
     }
 
     if is_prime(number) {
-        println!("{} is a number " ,number);
+        println!("{} is a prime number", number);
     } else {
-        println!("{} is not a prime number ", number);
+        println!("{} is not a prime number", number);
     }
 }
 
+fn get_input_as_u32() -> Option<u32> {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read input");
+
+    match input.trim().parse::<u32>() {
+        Ok(value) => Some(value),
+        Err(_) => None,
+    }
+}
+
+fn is_prime(n: u32) -> bool {
+    if n <= 1 {
+        return false;
+    }
+    if n <= 3 {
+        return true;
+    }
+    if n % 2 == 0 || n % 3 == 0 {
+        return false;
+    }
+    let mut i = 5;
+    while i * i <= n {
+        if n % i == 0 || n % (i + 2) == 0 {
+            return false;
+        }
+        i += 6;
+    }
+    true
+}
